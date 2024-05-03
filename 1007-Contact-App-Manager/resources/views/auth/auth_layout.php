112e7cdfa58ab0b3a744327227f04c1c76124202
AinulHanan <117377935+AinulHanan@users.noreply.github.com>
2024-05-03 23:14:46 +0700

    Add files via upload

---
+++ 1007-Contact-App-Manager/resources/views/auth/auth_layout.php
@@ -0,0 +1,22 @@
+<?php $title = 'Contact App'; ?>
+
+<?php
+ob_start();
+include '/assets/css/style.css';
+$style = ob_get_clean();
+?>
+<?php include_once 'resources/views/master-layout/master.php'; ?>
+
+<?php
+if (isset($_GET['auth'])) {
+    echo "<script>alert('Silahkan login terlebih dahulu');</script>";
+}
+if (isset($_GET['failed'])) {
+    echo "<script>alert('" . ucfirst($url) . " gagal');</script>";
+}
+if (isset($url)) {
+    include_once $url . '.php';
+}
+?>
+
+<?php include_once 'resources/views/master-layout/master.php'; ?>
