0742ea7747dc48d652ed2f9c38675c88cf11de04
AinulHanan <117377935+AinulHanan@users.noreply.github.com>
2024-05-24 20:55:13 +0700

    Update register.php

--- 1007-Contact-App-Manager/resources/views/auth/register.php
+++ 1007-Contact-App-Manager/resources/views/auth/register.php
@@ -1,14 +1,11 @@
- <section class="background-radial-gradient overflow-hidden">
-   <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+<section class="background-radial-gradient overflow-hidden">
+   <div class="container px-4 py-5 px-md-5 text-center text-lg-start" style="max-width: 100%; overflow-x: auto; overflow-y: auto; height: 100vh;">
      <div class="row gx-lg-5 align-items-center mb-5">
        <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
          <h1 class="mt-3 mb-3 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
            Selamat Datang Di<br />
            <span style="color: hsl(218, 81%, 95%)">Contact App Manager</span>
          </h1>
-         <!-- <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 95%)">
-           Store and Manage your Phone Contact here! -->
-         </p>
        </div>
        <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
          <div class="card bg-glass">
